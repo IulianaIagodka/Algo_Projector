6dbd0be83fccdfdd623e602b3b365d8e7c1dcb82
kiwi <kiwi@kiwis-MacBook-Pro.local>
2020-12-14 21:53:59 +0200

    add HM4 RGB

--- HM4_RGB.py
+++ HM4_RGB.py
@@ -1,4 +1,14 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
+"""
+The people of RGB Street have decided to paint each of their houses red, green, or blue.
+They've also decided that no two neighboring houses will be painted the same color.
+The neighbors of house i are houses i-1 and i+1. The first and last houses are not neighbors.
+You will be given three integer sequences R, G and B. Ri, Gi and Bi are the costs of painting the i-th house red,
+green, and blue, respectively.
+Find the minimal total cost required to perform the work.
+Input Format
+First line of input contains a single integer n — the number of houses. Each of the following n lines contains three integers Ri, Gi and Bi each.
+"""
 def minCost(costs):
     for i in range(1,len(costs)):
         x = costs[i]
